3fd0041629b8a6a9b1fbce2bbaa3c98cebce55a6
Sudo-devMan <inevitabledouble.e@gmail.com>
2025-08-01 18:27:01 +0200

    Connected PostgreSQL db on render

--- darentals_project/settings.py
+++ darentals_project/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for DaRentals project.
 
@@ -12,6 +14,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,10 +99,11 @@ WSGI_APPLICATION = 'darentals_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -139,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
