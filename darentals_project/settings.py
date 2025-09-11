e3caff423cd2464b2b1e6e8a43ee1edf01d02d15
Sudo-devMan <inevitabledouble.e@gmail.com>
2025-09-11 05:24:36 +0200

    Fixe allowed hosts

--- darentals_project/settings.py
+++ darentals_project/settings.py
@@ -98,25 +98,24 @@ WSGI_APPLICATION = 'darentals_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Add PostgreSQL With Railway
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydbname',
-#         'USER': 'da user',
-#         'PASSWORD': 'thepassword',
-#         'HOST': 'db url',
-#         'PORT': 'the port'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# PostgreSQL With Railway
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FvFOWftdHhxEFKFYnoTUMJbuxjHDAfOQ',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432'
+    }
+}
 # DATABASES['default']['PORT'] = 5432
 # if DATABASES['default'].get('PORT'):
 #     DATABASES['default']['PORT'] = 5432
