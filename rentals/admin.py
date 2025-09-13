497af4bdcead35e813b4bf0733f646e22cfd2ab2
Sudo-devMan <inevitabledouble.e@gmail.com>
2025-09-14 00:00:50 +0200

    Registered RentalRating

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
-from .models import Rental, RentalImage
+from .models import Rental, RentalImage, RentalRating
 
 admin.site.register(Rental)
 admin.site.register(RentalImage)
+admin.site.register(RentalRating)
